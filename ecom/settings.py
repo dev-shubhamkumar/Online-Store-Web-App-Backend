bd1f7a39b221d1e2ff7ac506c07c4c970f250c0a
Shubham Kumar <developer.shubham07@gmail.com>
2021-08-13 20:35:42 +0530

    adding serializers for CustomUser class

--- ecom/settings.py
+++ ecom/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'api',
     'api.category',
     'api.product',
+    # 'api.user'
 ]
 
 MIDDLEWARE = [
@@ -161,3 +162,5 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AUTH_USER_MODEL = 'user.CustomUser'
