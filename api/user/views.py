a4b3acbf34c2e29d5f8c4d1c36020113b1a7aa75
Shubham Kumar <developer.shubham07@gmail.com>
2021-08-13 21:30:27 +0530

    adding signin views function

--- api/user/views.py
+++ api/user/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
-
+from django.contrib.auth.backends import UserModel
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
+import random
+import re
 # Create your views here.
+
+
+def generate_session_token(length=10):
+    return ''.join(
+        random.SystemRandom().choice(
+            [chr(i) for i in range(97, 123)]
+            +
+            [str(i) for i in range(10)]
+        )
+        for _ in range(length)
+    )
+
+@csrf_exempt
+def signin(request):
+    if not request.method == 'POST':
+        return JsonResponse({
+            'error': 'Send a post request with valid parameters only.'
+        })
+    
+    username = request.POST['email']
+    password = request.POST['password']
+
+    ## validation part
+    if not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", username):
+        return JsonResponse({
+            'error': 'Enter a valid email'
+        })
+    
+    if len(password) < 8:
+        return JsonResponse({
+            'error': 'Password needs to be at least of 8 char'
+        })
+    
+
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(email=username)
+
+        if user.check_password(password):
+            usr_dict = UserModel.objects.filter(email=username).values().first()
+            usr_dict.pop('password')
+        
+            if user.session_token != '0':
+                user.session_token = '0'
+                user.save()
+                return JsonResponse({
+                    'error': 'Previous session exists'
+                })
+            
+            token = generate_session_token()
+            user.session_token = token
+            user.save()
+            login(request, user)
+            return JsonResponse({
+                'token': token, 
+                'user': usr_dict
+            })
+        
+        else:
+            return JsonResponse({
+                'error': 'Invalid Password'
+            })
+
+    except UserModel.DoesNotExist:
+        return JsonResponse({
+            'error': 'Invalid Email'
+        })
