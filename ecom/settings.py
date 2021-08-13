823c74e81644ee609d0f0686333a35d75dee9ac5
Shubham Kumar <developer.shubham07@gmail.com>
2021-08-13 12:19:23 +0530

    adding api base route

--- ecom/settings.py
+++ ecom/settings.py
@@ -38,11 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 
     ## Our added apps
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',     ## For custom signup functionality
+    'api',
 ]
 
 MIDDLEWARE = [
